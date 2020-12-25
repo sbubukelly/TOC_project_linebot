1ae64fe1202d39a2f06f0fc122ee0ab40b2c17a4
Kelly <sbubukellyg@gmail.com>
2020-12-26 03:23:28 +0800

    add smth

--- medLinebot/views.py
+++ medLinebot/views.py
@@ -7,7 +7,7 @@ from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
  
-from scraper import MyBest
+from .scraper import MyBest
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
