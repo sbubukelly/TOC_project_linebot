d936ab5945044baac3ec6c24520735f68b18e2fc
Kelly <sbubukellyg@gmail.com>
2020-12-27 19:42:01 +0800

    add smth

--- medLinebot/fsm.py
+++ medLinebot/fsm.py
@@ -5,9 +5,10 @@ class TocMachine(object):
    
     def __init__(self, **machine_configs):
         self.machine = Machine(model=self,use_pygraphviz=False, **machine_configs)
-
+'''
     def drawFSM(self):
         self.get_graph().draw('my_state_diagram.png', prog='dot')
+'''
 
 machine = TocMachine(
     states=["user", "Menu1", "Menu2","Menu3","subMenu","rank","purchase_link"],
@@ -44,7 +45,7 @@ machine = TocMachine(
     auto_transitions=False,
     show_conditions=True,
 )    
-machine.drawFSM()
+#machine.drawFSM()
 
 
         
