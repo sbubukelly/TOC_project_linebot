1f9da0d016013cfc7bddaf6ab2ce5097758a03f0
Kelly <sbubukellyg@gmail.com>
2020-12-27 20:44:11 +0800

    add smth

--- medLinebot/fsm.py
+++ medLinebot/fsm.py
@@ -1,14 +1,15 @@
-#from transitions import Machine
-from transitions.extensions import GraphMachine as Machine
+from transitions import Machine
+#from transitions.extensions import GraphMachine as Machine
 
 class TocMachine(object):
    
     def __init__(self, **machine_configs):
         self.machine = Machine(model=self,use_pygraphviz=False, **machine_configs)
 
+    '''
     def drawFSM(self):
         self.get_graph().draw('my_state_diagram.png', prog='dot')
-
+    '''
 
 machine = TocMachine(
     states=["user", "Menu1", "Menu2","Menu3","subMenu","rank","purchase_link","cancel"],
@@ -50,7 +51,7 @@ machine = TocMachine(
     auto_transitions=False,
     show_conditions=True,
 )    
-machine.drawFSM()
+#machine.drawFSM()
 
 
         
