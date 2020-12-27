b7f2cba8a5e52c15460b73d770d588e9ed1a422b
Kelly <sbubukellyg@gmail.com>
2020-12-27 19:46:10 +0800

    add smth

--- medLinebot/views.py
+++ medLinebot/views.py
@@ -20,7 +20,6 @@ from linebot.models import (
     URITemplateAction
 )
 from .scraper import MyBest
-from fsm import TocMachine
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
